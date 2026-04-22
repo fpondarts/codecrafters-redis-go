01f8f31d9ec6c9b4755d41caa997dceb9044eda2
fpondarts <perezflavio94@gmail.com>
2026-04-22 14:29:31 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,7 +5,70 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 )
+
+type TCPServer struct {
+	Listener   net.Listener
+	Clients    map[net.Conn]struct{}
+	ClientsMtx sync.Mutex
+	EventQueue chan struct{}
+}
+
+func (server *TCPServer) HandleConnection(conn net.Conn) {
+	server.ClientsMtx.Lock()
+	server.Clients[conn] = struct{}{}
+	server.ClientsMtx.Unlock()
+
+	defer func() {
+		conn.Close()
+		server.ClientsMtx.Lock()
+		delete(server.Clients, conn)
+		server.ClientsMtx.Unlock()
+	}()
+
+	buf := make([]byte, 512)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
+
+		data := string(buf[:n])
+
+		pingCount := strings.Count(data, "PING")
+
+		for range pingCount {
+			_, err = conn.Write([]byte("PONG\r\n"))
+			if err != nil {
+				return
+			}
+		}
+	}
+}
+
+type ServerConfig struct {
+	IP   net.IP
+	Port int
+}
+
+func NewTCPServer(config ServerConfig) (*TCPServer, error) {
+	addr := &net.TCPAddr{
+		IP:   config.IP,
+		Port: config.Port,
+	}
+
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start server")
+	}
+
+	return &TCPServer{
+		Listener:   listener,
+		Clients:    make(map[net.Conn]struct{}),
+		EventQueue: make(chan struct{}, 1000),
+	}, nil
+}
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var (
@@ -16,37 +79,24 @@ var (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
+	server, err := NewTCPServer(ServerConfig{
+		IP:   net.ParseIP("0.0.0.0"),
+		Port: 6379,
+	})
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l := server.Listener
+
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	for {
-		buf := make([]byte, 1024)
-		n, err := conn.Read(buf)
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error reading from connection at: ", conn.RemoteAddr())
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
-
-		data := buf[:n]
-
-		fmt.Println("Received: ", string(data))
-
-		nPings := strings.Count(string(data), "PING")
-
-		for range nPings {
-			_, err = conn.Write([]byte("+PONG\r\n"))
-			if err != nil {
-				fmt.Println("Failed to send response")
-			}
-			fmt.Println("Sending PONG")
-		}
+		server.HandleConnection(conn)
 	}
 }
